bdd174c45eae399e008f7b27f865d95de0ffb4e1
LeoKnox <LeoKnox@users.noreply.github.com>
2019-09-03 18:47:47 -0700

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 const routes = require('./routes');
+app.use(express.static('public'));
+
+app.get('/favicon.ico', (req, res, next) => {
+    return res.sendStatus(204);
+});
 
 app.use('/', routes());
 
